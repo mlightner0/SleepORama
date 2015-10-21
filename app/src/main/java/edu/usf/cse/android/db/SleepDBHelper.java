66f3c85768612481ad30af191d7e4cd567b735b5
alexboes <alex_boes@verizon.net>
2015-10-21 12:02:29 -0400

    Fixed errors

--- app/src/main/java/edu/usf/cse/android/db/SleepDBHelper.java
+++ app/src/main/java/edu/usf/cse/android/db/SleepDBHelper.java
@@ -8,6 +8,19 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by Alex on 10/20/2015.
  */
 public class SleepDBHelper extends SQLiteOpenHelper {
+    private static final String CREATE_PREFERENCES_TABLE =
+            "create table preferences(" +
+            "p_id integer primary key autoincrement, " +
+            "information text not null)";
+    private static final String CREATE_SESSIONS_TABLE =
+            "create table sessions(" +
+            "session_id integer primary key autoincrement, " +
+            "date text not null)";
+    private static final String CREATE_DATAPOINTS_TABLE =
+            "create datapoints table(" +
+            "d_id integer primary key autoincrement, " +
+            "session_id integer not null, " +
+            "datapoint real not null)";
 
     private static final String DB_NAME = "SleepORama";
     private static final int DB_VERSION = 1;
@@ -18,11 +31,16 @@ public class SleepDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_PREFERENCES_TABLE);
+        db.execSQL(CREATE_SESSIONS_TABLE);
+        db.execSQL(CREATE_DATAPOINTS_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS preferences");
+        db.execSQL("DROP TABLE IF EXISTS sessions");
+        db.execSQL("DROP TABLE IF EXISTS datapoints");
         onCreate(db);
     }
 }
--- app/src/main/java/edu/usf/cse/android/db/SleepDBManager.java
+++ app/src/main/java/edu/usf/cse/android/db/SleepDBManager.java
@@ -1,5 +1,6 @@
 package edu.usf.cse.android.db;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -24,5 +25,44 @@ public class SleepDBManager {
 
     public void close(){
         dbHelper.close();
+    }
+
+    private ContentValues createPreferencesValues(String information) {
+        ContentValues values = new ContentValues();
+        values.put("information", information);
+        return values;
+    }
+
+    private ContentValues createSessionsValues(String date){
+        ContentValues values = new ContentValues();
+        values.put("date", date);
+        return values;
+    }
+
+    private ContentValues createDatapointsValues(long session_id, double datapoint){
+        ContentValues values = new ContentValues();
+        values.put("session_id", session_id);
+        values.put("datapoint", datapoint);
+        return values;
+    }
+
+    public long createPreference(String information){
+        ContentValues initialValues = createPreferencesValues(information);
+        return db.insert("preferences", null, initialValues);
+    }
+
+    public long createSession(String date){
+        ContentValues initialValues = createSessionsValues(date);
+        return db.insert("sessions", null, initialValues);
+    }
+
+    public long createDatapoint(Long session_id, double datapoint){
+        ContentValues initialValues = createDatapointsValues(session_id, datapoint);
+        return db.insert("datapoints", null, initialValues);
+    }
+
+    public boolean updatePreference(long p_id, String information){
+        ContentValues updateValues = createPreferencesValues(information);
+        return db.update("preferences", updateValues, "p_id =" + p_id, null) > 0;
     }
 }
