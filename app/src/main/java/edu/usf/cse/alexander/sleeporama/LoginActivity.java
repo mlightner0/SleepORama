b5dc2460628d3952d1c80722d2cbc59a07bd5f38
mlightner0 <mlightner@mail.usf.edu>
2015-10-21 22:45:19 -0400

    New LoginActivity.java

--- app/src/main/java/edu/usf/cse/alexander/sleeporama/LoginActivity.java
+++ app/src/main/java/edu/usf/cse/alexander/sleeporama/LoginActivity.java
@@ -1,16 +1,39 @@
 package edu.usf.cse.alexander.sleeporama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button) this.findViewById(R.id.Login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                login(view);
+            }
+        });
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        Bundle bundle = new Bundle();
+        EditText editText = (EditText) findViewById(R.id.username);
+        String username = editText.getText().toString();
+        bundle.putString("EXTRA_MESSAGE", username);
+        intent.putExtras(bundle);
+        startActivity(intent);
     }
 
     @Override
@@ -35,5 +58,7 @@ public class LoginActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
 
 
